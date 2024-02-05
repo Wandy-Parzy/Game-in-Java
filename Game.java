2ae05b61fe23cb076661eb185a932cab5e75521e
Wandy-Parzy <wandypaulino05@gmail.com>
2024-02-04 21:14:51 -0400

    First commit

---
+++ Game.java
@@ -0,0 +1,20 @@
+ 
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author wandy
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame miventana = new Ventana();
+        miventana.setVisible(true);
+    }
+    
+    
+}
